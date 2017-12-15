4f723c93acfff9efb276de538d33fdf175c71680
Hoang Viet <davishoang96@gmail.com>
2017-12-16 02:51:28 +0700

    ConsoleChat

---
+++ ConsoleChat/Client/Client/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Client
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            client();
+        }
+
+        public static void client()
+        {
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                server.Connect(ip); //Connect to the server
+            }
+            catch (SocketException ex)
+            {
+                //Console.WriteLine(ex.Message);
+                Console.WriteLine("Server not found");
+                Thread.Sleep(2000);
+                client();
+                //return;
+            }
+
+            Console.WriteLine("Connected to server ");
+
+            while (true)
+            {
+
+                try
+                {
+                    byte[] data = new byte[1024];
+                    string input;
+                    int receivedDataLength = server.Receive(data); //Wait for the data
+                    string stringData = Encoding.ASCII.GetString(data, 0, receivedDataLength); //Decode the data received
+                    Console.WriteLine(stringData); //Write the data on the screen
+
+                    if (stringData == "REQ_STOP")
+                    {
+                        input = "ALL STOP";
+                        Thread.Sleep(1000);
+                        server.Send(Encoding.ASCII.GetBytes(input));
+
+                        Thread.Sleep(1000);
+                        break;
+                    }
+
+                    if (stringData == "REQ_SPY")
+                    {
+                        input = "SPY";
+                        Thread.Sleep(5000);
+                        server.Send(Encoding.ASCII.GetBytes(input));
+                    }
+
+                    if (stringData == "REQ_PB")
+                    {
+                        input = "PB";
+                        Thread.Sleep(1000);
+                        server.Send(Encoding.ASCII.GetBytes(input));
+                    }
+
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    break;
+                }
+
+            }
+
+            Console.WriteLine("STOP");
+
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
+
+            //server.Shutdown(SocketShutdown.Both);
+            //server.Close();
+        }
+    }
+}
---
+++ ConsoleChat/Server/Server/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            server();
+        }
+
+        public static void server()
+        {
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, 8080); //Any IPAddress that connects to the server on any port
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); //Initialize a new Socket
+
+            socket.Bind(ip); //Bind to the client's IP
+            socket.Listen(3); //Listen for maximum 10 connections
+            Console.WriteLine("Waiting for a client...");
+            Socket client = socket.Accept();
+            IPEndPoint clientep = (IPEndPoint)client.RemoteEndPoint;
+            Console.WriteLine("Connected with {0} at port {1}", clientep.Address, clientep.Port);
+
+            try
+            {
+
+                while (true)
+                {
+                    string input = Console.ReadLine(); //This is the data we will respond with
+                    if (input == "exit")
+                        break;
+                    byte[] data = new byte[1024];
+                    data = Encoding.ASCII.GetBytes(input); //Encode the data
+                    client.Send(data, data.Length, SocketFlags.None); //Send the data to the client
+                    int dataReceived = client.Receive(data);
+                    string stringData = Encoding.ASCII.GetString(data, 0, dataReceived); //Decode the data received
+                    Console.WriteLine(stringData); //Write the data on the screen
+                }
+
+
+                Console.WriteLine("Disconnected from {0}", clientep.Address);
+                client.Close(); //Close Client
+                socket.Close(); //Close socket
+
+
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine(se.Message);
+            }
+
+
+            //Console.WriteLine("Disconnected from {0}", clientep.Address);
+            //client.Close(); //Close Client
+            //socket.Close(); //Close socket
+        }
+    }
+}
